ef70a8ef6925765e3edc18cb0c7ef11be934c187
inoninja <88498417+inoninja@users.noreply.github.com>
2025-09-23 21:32:30 +0800

    change

--- public/pages/signup.php
+++ public/pages/signup.php
@@ -1,7 +1,7 @@
 <?php
 //signup.php
 session_start();
-require_once __DIR__ . '/../database/db.php';
+require_once('../../database/db.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Input validation and sanitization
