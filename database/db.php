6b2c64cf89ac73f20c0aa86488fd6f4be372860e
inoninja <88498417+inoninja@users.noreply.github.com>
2025-09-27 00:48:55 +0800

    Update db.php

--- database/db.php
+++ database/db.php
@@ -14,9 +14,8 @@ try {
     error_log("DB_NAME: " . $dbname);
     error_log("DB_USER: " . $user);
     error_log("DB_PASS: " . $password);
-    error_log("DB_ENDPOINT: " . $endpoint);
 
-    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require;options=endpoint=$endpoint", $user, $password);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
